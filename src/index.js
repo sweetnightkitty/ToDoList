dd883bb414efe49fb12402facfb49e9904762bdf
SaraToth <saratoth101@gmail.com>
2024-08-27 19:31:13 +0900

    Complete the logic for taskSubmitBtn event listener to take in the form input and process it to a new task, and submit to current project and resets and closes the modal

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import "./styles.css";
 import { openModal } from "./dom";
 import { closeModal } from "./dom";
 import { resetForm } from "./dom";
-import { createProject } from "./tasks";
+import { taskManager } from "./tasks";
+
+const manage = taskManager();
 
 //Main Buttons
 const addProjectBtn = document.querySelector(".btn-add-project");
 const addTaskBtn = document.querySelector(".btn-add-task");
 
 //Modal Buttons
-const projectModalBtn = document.querySelector(".btn-submit-modal-project");
-const taskModalBtn = document.querySelector(".btn-submit-modal-task");
+const projectSubmitBtn = document.querySelector(".btn-submit-modal-project");
+const taskSubmitBtn = document.querySelector(".btn-submit-modal-task");
 const closeModalBtns = document.querySelectorAll(".btn-close");
 
 
@@ -26,24 +28,31 @@ addTaskBtn.addEventListener("click", () => {
     openModal(modal);
 });
 
-projectModalBtn.addEventListener("click", (e) => {
+projectSubmitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    createProject();
-    //update left column?
+    manage.createProject();
+    //update left column
     closeModal();
     resetForm();
     
 });
 
-taskModalBtn.addEventListener("click", (e) => {
+taskSubmitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    //stuff
+    const newTask = manage.createTask();
+    const currentProject = manage.getCurrentProject();
+    manage.addTaskToProject(newTask, currentProject);
+    closeModal();
+    resetForm();
+    console.log(manage.getProjects());
+
 });
 
 
 closeModalBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         closeModal();
+        resetForm();
     });
 });
 
@@ -62,20 +71,20 @@ const projectsContainer = document.querySelector(".container-left");
 const tasksContainer = document.querySelector(".container-right");
 
 //Example projects
-const projects = [
-    {
-        name: "default",
-        tasks: [],
-    },
+// const projects = [
+//     {
+//         name: "default",
+//         tasks: [],
+//     },
 
-    {
-        name: "languages",
-        tasks: [],
-    },
-];
+//     {
+//         name: "languages",
+//         tasks: [],
+//     },
+// ];
 
-projects[0].tasks.push("make this work");
-projects[1].tasks.push("review japanese");
+// projects[0].tasks.push("make this work");
+// projects[1].tasks.push("review japanese");
 
-console.log(projects[0]);
-console.log(projects[1]);
+// console.log(projects[0]);
+// console.log(projects[1]);
