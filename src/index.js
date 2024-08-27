d65251078f8b04c53b7161886a6f7bcd3356e7fb
SaraToth <saratoth101@gmail.com>
2024-08-27 18:25:39 +0900

    Create event listeners for add project, add task, and modal close buttons, and assign the relavent close or open modal functions to run on click, to create functionality that opens and closes modals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
 import "./styles.css";
+import { openModal } from "./dom";
+import { closeModal } from "./dom";
+
+const addProjectBtn = document.querySelector(".btn-add-project");
+const addTaskBtn = document.querySelector(".btn-add-task");
+const closeModalBtns = document.querySelectorAll(".btn-close");
+
+addProjectBtn.addEventListener("click", () => {
+    //selects project modal
+    const modal = document.querySelector(".modal-project");
+    openModal(modal);
+});
+
+addTaskBtn.addEventListener("click", () => {
+    //selects task modal
+    const modal = document.querySelector(".modal-task");
+    openModal(modal);
+});
+
+closeModalBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        closeModal();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+//Examples:
+
+const projectsContainer = document.querySelector(".container-left");
+const tasksContainer = document.querySelector(".container-right");
 
 //Example projects
 const projects = [
