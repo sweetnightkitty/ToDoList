cdfcbbe041becff1abf33ddd04cc98f1586b3325
SaraToth <saratoth101@gmail.com>
2024-08-27 18:43:38 +0900

    Create function that will take the project name user types in and add new project object to the projects array which is yet to be created

---
+++ src/tasks.js
@@ -0,0 +1,10 @@
+export function createProject() {
+    const projectName = document.querySelector("#project-name").value;
+    const newProject = {
+        name: projectName,
+        type: [],
+    };
+
+    //projects.push(newProject);
+
+};
