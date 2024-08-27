24cf1c1daff7185fc6191a2fd3ea4418589c9947
SaraToth <saratoth101@gmail.com>
2024-08-27 17:02:26 +0900

    Create basic html structure, and basic style in css for visualization, and create an example array of objects in index which will serve as the building point to create and store different projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+//Example projects
+const projects = [
+    {
+        name: "default",
+        tasks: [],
+    },
+
+    {
+        name: "languages",
+        tasks: [],
+    },
+];
+
+projects[0].tasks.push("make this work");
+projects[1].tasks.push("review japanese");
+
+console.log(projects[0]);
+console.log(projects[1]);
