6a5f9693df34ee751d70db0cbfa3d3eeba491ac5
SaraToth <saratoth101@gmail.com>
2024-08-27 19:30:06 +0900

    Convert functions into factory function, create logic to get the current project, switch projects, update create task const to correctly target radio buttons, create function to add task to project

--- src/tasks.js
+++ src/tasks.js
@@ -1,10 +1,69 @@
-export function createProject() {
-    const projectName = document.querySelector("#project-name").value;
-    const newProject = {
-        name: projectName,
-        type: [],
+export function taskManager() {
+    const projects = [
+        {
+            name: "Main",
+            tasks: [],
+        },
+    ];
+
+    const getProjects = () => projects;
+
+    let currentProject = projects[0];
+
+    const switchProjects = (projectName) => {
+        for(let i = 0; i < projects.length; i++) {
+            if(projects[i].name == projectName) {
+                currentProject = projects[i];
+            };
+        };
     };
 
-    //projects.push(newProject);
+    const createProject = () => {
+        const projectName = document.querySelector("#project-name").value;
+        const newProject = {
+            name: projectName,
+            tasks: [],
+        };
+    
+        projects.push(newProject);
+    };
+    
+    
+    const createTask = () => {
+        const title = document.querySelector("#task-name").value;
+        const description = document.querySelector("#task-descript").value;
+        const date = document.querySelector("#task-date").value;
+        let priority;
 
+        const priorities = document.getElementsByName("task-priority");
+        priorities.forEach((option) => {
+            if(option.checked) {
+                priority = option.id;
+            };
+        });
+    
+        const newTask = {
+            title: title,
+            description: description,
+            date: date,
+            priority: priority,
+        };
+    
+        return newTask;
+    };
+
+    const getCurrentProject = () => currentProject;
+
+    const addTaskToProject = (newTask, currentProject) => {
+        currentProject.tasks.push(newTask);
+    };
+
+    return {
+        getProjects,
+        createProject,
+        createTask,
+        getCurrentProject,
+        switchProjects,
+        addTaskToProject,
+    }
 };
