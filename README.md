2dd8b491970ed54542a35e7d694bbab7bc6df871
SaraToth <saratoth101@gmail.com>
2024-08-31 15:55:39 +0900

    create function that takes in a task, and appends all the task properties as seperate divs to a card div, adds a delete button, and then displays this card in the UI to represent a to do list task, add border to the card in stylesheet and update README

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 https://www.theodinproject.com/lessons/node-path-javascript-todo-list
 
 ## Known problems or tasks to complete
-* the createTask function does not work as is if added to the window event listener and needs to be modified in order to display previous written tasks.
 * Currently task titles are added as div content and displayed on UI, but need to be converted into a card with checkbox and delete button / right click delete.
-* projects are added to the UI as textContent, but should be converted into buttons
-    * Button id or class will trigger switch project function
+
+* projects are added to the UI as buttons
+    * Button class will trigger switch project function
     * re displayTasks to repopulate right column content
--- src/tasks-dom.js
+++ src/tasks-dom.js
@@ -6,9 +6,19 @@ export function displayTasks(currentProject) {
 
     const array = currentProject.tasks;
     for(let i = 0; i < array.length; i++) {
-        const task = document.createElement("div");
-        task.textContent = array[i].title;
-        tasksContainer.appendChild(task);
+        const task = array[i];
+
+        // const temp = document.createElement("div");
+        // temp.textContent = array[i].title;
+        // tasksContainer.appendChild(temp);
+
+        const card = createCard(task);
+        tasksContainer.appendChild(card);
+
+        //create a card = div
+        //append info to card in seperate divs
+        //append a delet button
+        //append card to tasksContainer
     };
 };
 
@@ -16,3 +26,38 @@ export function displayTasks(currentProject) {
 function clearTasksContainer() {
     tasksContainer.innerHTML = "";
 };
+
+function createCard(task) {
+    //makes card div
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const title = createDiv(task.title);
+    const description = createDiv(task.description);
+    const date = createDiv(task.date);
+    const priority = createDiv(task.priority);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn-taskName"); //need a unique class identifier
+    deleteBtn.textContent = "Delete";
+
+    card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(date);
+    card.appendChild(priority);
+    card.appendChild(deleteBtn);
+
+    return card;
+};
+
+// const newTask = {
+//     title: title,
+//     description: description,
+//     date: date,
+//     priority: priority,
+// }
+
+function createDiv(textContent) {
+    const div = document.createElement("div");
+    div.textContent = textContent;
+    return div;
+}
