b4ab4e63bbcd7eae80295758e341c2be5612d212
SaraToth <saratoth101@gmail.com>
2024-09-07 15:30:58 +0900

    Config eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import webpackConfig from "./webpack.config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  webpackConfig,
+  eslintConfigPrettier,
+];
